fbc777ec94b9669eacac829386a5c9ad412961d5
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-07-17 11:20:56 +0300

    Catch ids

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 
-
+// catch ids
+const cowHouse1 = document.getElementById("cat1");
+const cowHouse2 = document.getElementById("cat2");
+const cowHouse3 = document.getElementById("cat3");
+const cowHouse4 = document.getElementById("cat4");
