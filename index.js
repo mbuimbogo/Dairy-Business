b8cc2a6c83f1552696d8721e385bca3b8c78d511
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-07-17 23:20:43 +0300

    get totals function

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cowHouse4 = document.getElementById("cat4");
 const totalAmount = document.getElementById("total");
 const weeklyIncome = document.getElementById("weekly");
 const yearlyIncome = document.getElementById("yearly");
-const eachMonth = document.getElementById("list");
+const singleMonths = document.getElementById("list");
 
 document.getElementById(form.addEventListener("submit",getTotals));
 document.getElementById(form.addEventListener("submit",weeklyIncome));
@@ -15,12 +15,12 @@ document.getElementById(form.addEventListener("submit",monthlyIncome));
 document.getElementById(form.addEventListener("submit",yearlyIncome));
 
 // Total milk amount in litres
-
-
-
+function getTotals() {
+    const final = (cowHouse1.value)+(cowHouse2.value)+(cowHouse3.value)+(cowHouse4.value)
+}
 
 // Listing individual months
-eachMonth.innerHTML =
+singleMonths.innerHTML=
         <ul>
             <li class="list">Income for ${months[0]} is</li>
             <li class="list">Income for ${months[1]} is</li>
