b69bfe09d543b194cd427da797fc441925749ebd
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-07-17 15:12:36 +0300

    AddEventListener

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const cowHouse1 = document.getElementById("cat1");
 const cowHouse2 = document.getElementById("cat2");
 const cowHouse3 = document.getElementById("cat3");
 const cowHouse4 = document.getElementById("cat4");
+const totalAmount = document.getElementById("total");
+const weeklyIncome = document.getElementById("weekly");
+const yearlyIncome = document.getElementById("yearly");
+
+
+document.getElementById(form.addEventListener("submit",getTotals));
+document.getElementById(form.addEventListener("submit",weeklyIncome));
+document.getElementById(form.addEventListener("submit",monthlyIncome));
+document.getElementById(form.addEventListener("submit",yearlyIncome));
+
