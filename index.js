fea7c0c6e4ba087ac02dc96c5360d8b80bcb966e
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-07-17 20:07:07 +0300

    add monthList

--- index.js
+++ index.js
@@ -7,10 +7,32 @@ const cowHouse4 = document.getElementById("cat4");
 const totalAmount = document.getElementById("total");
 const weeklyIncome = document.getElementById("weekly");
 const yearlyIncome = document.getElementById("yearly");
-
+const eachMonth = document.getElementById("list");
 
 document.getElementById(form.addEventListener("submit",getTotals));
 document.getElementById(form.addEventListener("submit",weeklyIncome));
 document.getElementById(form.addEventListener("submit",monthlyIncome));
 document.getElementById(form.addEventListener("submit",yearlyIncome));
+
+// Total milk amount in litres
+
+
+
+
+// Listing individual months
+eachMonth.innerHTML =
+        <ul>
+            <li class="list">Income for ${months[0]} is</li>
+            <li class="list">Income for ${months[1]} is</li>
+            <li class="list">Income for ${months[2]} is</li>
+            <li class="list">Income for ${months[3]} is</li>
+            <li class="list">Income for ${months[4]} is</li>
+            <li class="list">Income for ${months[5]} is</li>
+            <li class="list">Income for ${months[6]} is</li>
+            <li class="list">Income for ${months[7]} is</li>
+            <li class="list">Income for ${months[8]} is</li>
+            <li class="list">Income for ${months[9]} is</li>
+            <li class="list">Income for ${months[10]} is</li>
+            <li class="list">Income for ${months[11]} is</li>
+        </ul>
 
